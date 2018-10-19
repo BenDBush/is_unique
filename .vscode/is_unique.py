e2673dd782712ae7a67a3020a4484b3db268bbb3
Benjamin Bush <benjamin.bush15@houghton.edu>
2018-10-19 12:07:42 -0400

    Updated to check not already seen, rather than not in future

--- .vscode/is_unique.py
+++ .vscode/is_unique.py
@@ -5,20 +5,15 @@ from hypothesis import strategies as st
 
 def check_unique_chars_in_string(string_to_check):
     """returns True if all chars are unique, False otherwise"""
-    for i in range(len(string_to_check)):
-        if check_matches_char_in_str(string_to_check[i], string_to_check[i+1:]):  # check whether i occurs later in string_to_check returned true? 
+    store = []
+    for i in string_to_check:
+        if i in store:
             return False
-    else:
-        return True
+        else:
+            store.append(i)
 
+    return True
 
-def check_matches_char_in_str(target_character, match_string):
-    for i in match_string:
-        if target_character == i:
-            return True
-
-    
-    return False  # exited for loop without finding another occurence of target_character
 
 
 class TestUniqCharInString(unittest.TestCase):
