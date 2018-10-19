e39b621f7cf15ceb4d8180707d8ce1f923b17061
Benjamin Bush <benjamin.bush15@houghton.edu>
2018-10-19 08:39:49 -0400

    updated test suite to include several with predefined strings, one which is a smoke test.

--- .vscode/is_unique.py
+++ .vscode/is_unique.py
@@ -23,8 +23,25 @@ def check_matches_char_in_str(target_character, match_string):
 
 class TestUniqCharInString(unittest.TestCase):
     @given(test_string = st.text())
-    def test_unique_chars(self, test_string):
+    def test_unique_chars_crash(self, test_string):
         check_unique_chars_in_string(test_string)
+
+
+    def test_unique_string(self):
+        self.assertTrue(check_unique_chars_in_string("abcdefghijklmnopqrstuvwxyz"))
+
+
+    def test_repeated_char_at_end(self):
+        self.assertFalse(check_unique_chars_in_string("abcdefghijklmnopqrstuvwxyza"))
+
+
+    def test_doubled_char_in_string(self):
+        self.assertFalse(check_unique_chars_in_string("abcdefghijklmmnopqrstuvwxyz"))
+
+
+    def test_repeated_char_middles(self):
+        self.assertFalse(check_unique_chars_in_string("abcdefghijklmnopqristuvwxyz"))
+
 
 if __name__ == '__main__':
     unittest.main()
